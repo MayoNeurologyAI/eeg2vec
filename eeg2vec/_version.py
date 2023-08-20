d55381f8cbd9536ee265deb962759882b658e3a9
Sampath Gogineni <sampath.gogineni@gmail.com>
2023-08-20 10:18:15 -0500

    added resampling and nan identification in eegs

--- eeg2vec/_version.py
+++ eeg2vec/_version.py
@@ -1,4 +1,5 @@
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 
 #change log:
 # 0.0.0: Pre-processing includes min-max normalization and scaling
+# 0.0.1: added interpolation for resapling and nan annotations
