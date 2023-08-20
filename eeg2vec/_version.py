a5a68f2b4c8d6df99fc8b401fffbd657d05fa4c9
Sampath Gogineni <sampath.gogineni@gmail.com>
2023-08-20 16:23:44 -0500

    downgraded pandas version to work with silver

--- eeg2vec/_version.py
+++ eeg2vec/_version.py
@@ -1,6 +1,7 @@
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 #change log:
 # 0.0.0: Pre-processing includes min-max normalization and scaling
 # 0.0.1: added interpolation for resapling and nan annotations
 # 0.0.2: Modified environment.yml to include latest packages
+# 0.0.3: reverted pandas version to 1.5.3
