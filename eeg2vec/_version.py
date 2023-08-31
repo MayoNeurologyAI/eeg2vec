3dd07e9bd2fb956fca35e4d240754853663d0aca
Sampath Gogineni <sampath.gogineni@gmail.com>
2023-08-31 15:50:33 -0500

    added collate function

--- eeg2vec/_version.py
+++ eeg2vec/_version.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 #change log:
 # 0.0.0: Pre-processing includes min-max normalization and scaling
@@ -6,3 +6,4 @@ __version__ = '0.1.0'
 # 0.0.2: Modified environment.yml to include latest packages
 # 0.0.3: reverted pandas version to 1.5.3
 # 0.1.0: added bendr models, dataset and transforms
+# 0.1.0: added collate function for epochs
--- eeg2vec/dataset/_collate.py
+++ eeg2vec/dataset/_collate.py
@@ -3,9 +3,10 @@ import numpy as np
 from joblib import Parallel, delayed
 
 class CollateEpochs(object):
-    def __init__(self, transform):
+    def __init__(self, transform, jobs=-1):
         self.transform = transform
+        self.jobs = jobs
     
     def __call__(self, batch):
-        transforms = Parallel(n_jobs=-1)(delayed(self.transform)(sample) for sample in batch)
+        transforms = Parallel(n_jobs=self.jobs)(delayed(self.transform)(sample) for sample in batch)
         return torch.from_numpy(np.stack(transforms))
